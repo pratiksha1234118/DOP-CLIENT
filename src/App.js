a5a4d8d9359209dafe76a7c7a56d12f220753932
Pratiksha Gavande <62094479+pratiksha1234118@users.noreply.github.com>
2022-06-01 16:06:04 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import { DopAccountUpdate } from './ComponentCustomer/DopAccountUpdate';
 import DopViewAccounts from './ComponentCustomer/DopViewAccounts';
 import { Routes, Route, Link,BrowserRouter } from "react-router-dom";
 import {CustomerLogin} from './ComponentCustomer/CustomerLogin';
+import {AdminLogin} from './ComponentCustomer/AdminLogin';
 function App() {
   return (
-    <div>
-    <Routes>
-    <Route  path='/' element={<DopAccountUpdate/>}></Route>
-    <Route  path='/viewAccount' element={<DopViewAccounts/>}></Route>
-    </Routes>
    
-    </div>
+    <Routes>
+    
+      <Route  path='/dopAccountUpdate' element={<DopAccountUpdate/>}></Route>
+      <Route  path='/viewAccount' element={<DopViewAccounts/>}></Route>    
+     <Route  path='/' element={<CustomerLogin/>}></Route>
+     <Route  path='/loginAdmin' element={<AdminLogin/>}></Route>
+     
+     </Routes>
+   
+
   );
 }
 
